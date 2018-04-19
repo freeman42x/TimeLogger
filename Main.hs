4bf3943ced46b672202a12f8ce4e2a723811bca5
Răzvan Flavius Panda <razvan.panda@gmail.com>
2018-04-19 02:07:04 +0100

    $

--- Main.hs
+++ Main.hs
@@ -7,12 +7,7 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
-
-
 module Main where
-
--- https://www.schoolofhaskell.com/school/starting-with-haskell/libraries-and-frameworks/persistent-db#creating-a-database
-
 
 import           Control.Concurrent
 import           Data.Time.Clock
@@ -30,23 +25,6 @@ import           Database.Persist.Sqlite  (runMigration, runSqlite)
 import           Database.Persist.TH      (mkMigrate, mkPersist,
                                            persistLowerCase, share, sqlSettings)
 
-main :: IO ()
-main = do
-  d <- openDisplay ""
-  loop d
-
-loop :: Display -> IO ()
-loop d = do
-  time <- getCurrentTime
-  (w, _) <- getInputFocus d
-  a <- internAtom d "_NET_WM_NAME" False
-  p <- getTextProperty d w a
-  ps <- wcTextPropertyToTextList d p
-  windowAttrs <- getWindowAttributes d w
-  print $ show time ++ " Name: " ++ show ps ++ " Width: " ++ show (wa_width windowAttrs)
-  threadDelay 1000000
-  loop d
-
 share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
 LogItem
    title    Text
@@ -55,12 +33,25 @@ LogItem
    deriving Show
 |]
 
-main3 :: IO ()
-main3 =
+main :: IO ()
+main =
   runSqlite "db.sqlite" $ do
     runMigration migrateTables
-
-    tuts <- select $ from $ \l -> do
+    logItem <- select $ from $ \l -> do
             orderBy [desc (l ^. LogItemId)]
             return (l ^. LogItemId, l ^. LogItemTitle)
-    liftIO $ print tuts
+    liftIO $ do
+      print logItem
+      d <- openDisplay ""
+      loop d
+
+loop :: Display -> IO ()
+loop d = do
+  time <- getCurrentTime
+  (w, _) <- getInputFocus d
+  a <- internAtom d "_NET_WM_NAME" False
+  p <- getTextProperty d w a
+  ps <- wcTextPropertyToTextList d p
+  print $ show time ++ " Name: " ++ show ps
+  threadDelay 1000000
+  loop d
